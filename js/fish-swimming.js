daec3860e7f2b1a7d4cd808a2cb6931fbe3f5c76
Anne-Sofie123 <annesofiepoulsen568@gmail.com>
2026-05-20 15:34:41 +0200

    fået alle lyde til at passe til fiskene

--- js/fish-swimming.js
+++ js/fish-swimming.js
@@ -2,21 +2,21 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const fishPairs = [
-        { fish: ".fish1", fakta: ".fish1-fakta", sound: "sounds/søpindsvin.mp3" },
-        { fish: ".fish2", fakta: ".fish2-fakta", sound: "sounds/søhest.mp3" },
-        { fish: ".fish3", fakta: ".fish3-fakta", sound: "sounds/skildpade.mp3" },
-        { fish: ".fish4", fakta: ".fish4-fakta", sound: "sounds/havsnegl.mp3" },
-        { fish: ".fish5", fakta: ".fish5-fakta", sound: "sounds/haj-lilla.mp3" },
-        { fish: ".fish6", fakta: ".fish6-fakta", sound: "sounds/haj-blå.mp3" },
-        { fish: ".fish7", fakta: ".fish7-fakta", sound: "sounds/krabbe.mp3" },
-        { fish: ".fish8", fakta: ".fish8-fakta", sound: "sounds/havfruen.mp3" },
-        { fish: ".fish9", fakta: ".fish9-fakta", sound: "sounds/guldfisk.mp3" },
-        { fish: ".fish10", fakta: ".fish10-fakta", sound: "sounds/haj-grøn.mp3" },
-        { fish: ".fish11", fakta: ".fish11-fakta", sound: "sounds/blæksprut-grøn.mp3" },
-        { fish: ".fish12", fakta: ".fish12-fakta", sound: "sounds/delin.mp3" },
-        { fish: ".fish13", fakta: ".fish13-fakta", sound: "sounds/blæksprut-lyserød.mp3" }
-    ];
+   const fishPairs = [
+    { fish: ".fish1", fakta: ".fish1-fakta", sound: "sounds/søpindsvin.mp3" },
+    { fish: ".fish2", fakta: ".fish2-fakta", sound: "sounds/søhest.mp3" },
+    { fish: ".fish3", fakta: ".fish3-fakta", sound: "sounds/skildpadde.mp3" },
+    { fish: ".fish4", fakta: ".fish4-fakta", sound: "sounds/havsnegl.mp3" },
+    { fish: ".fish5", fakta: ".fish5-fakta", sound: "sounds/haj-lilla.mp3" },
+    { fish: ".fish6", fakta: ".fish6-fakta", sound: "sounds/haj-blå.mp3" },
+    { fish: ".fish7", fakta: ".fish7-fakta", sound: "sounds/krabbe.mp3" },
+    { fish: ".fish8", fakta: ".fish8-fakta", sound: "sounds/havfruen.mp3" },
+    { fish: ".fish9", fakta: ".fish9-fakta", sound: "sounds/guldfisk.mp3" },
+    { fish: ".fish10", fakta: ".fish10-fakta", sound: "sounds/haj-grøn.mp3" },
+    { fish: ".fish11", fakta: ".fish11-fakta", sound: "sounds/blæksprut-grøn.mp3" },
+    { fish: ".fish12", fakta: ".fish12-fakta", sound: "sounds/delfin.mp3" },
+    { fish: ".fish13", fakta: ".fish13-fakta", sound: "sounds/blæksprut-lyserød.mp3" }
+];
 
     let activeFakta = null;
     let activeFish = null;
@@ -30,18 +30,15 @@ document.addEventListener("DOMContentLoaded", () => {
 
         fish.addEventListener("click", () => {
 
-            // skjul gammel fakta-boks
             if (activeFakta && activeFakta !== fakta) {
                 activeFakta.style.display = "none";
             }
 
-            // stop gammel lyd
             if (activeAudio) {
                 activeAudio.pause();
                 activeAudio.currentTime = 0;
             }
 
-            // hvis samme fisk trykkes igen
             if (fakta.style.display === "block") {
                 fakta.style.display = "none";
                 activeFakta = null;
@@ -52,7 +49,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 activeFakta = fakta;
                 activeFish = fish;
 
-                // start ny lyd
                 activeAudio = new Audio(pair.sound);
                 activeAudio.play();
             }
