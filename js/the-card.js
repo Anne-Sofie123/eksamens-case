2933ea71a58795e75ff860fdb03f56b7eb4b8eaa
Anne-Sofie123 <annesofiepoulsen568@gmail.com>
2026-05-24 12:30:55 +0200

    lavet kommentar til The cards" js

--- js/the-card.js
+++ js/the-card.js
@@ -1,31 +1,58 @@
+// Finder alle cirkler på kortet //
 const circles = document.querySelectorAll(".map-circle");
 
+ // Skjuler alle cirkler //
 function hideCircles() {
+
+    // Tager hver cirkel én ad gangen //
     circles.forEach(circle => {
+
+        // Gør cirklen usynlig //
         circle.style.display = "none";
     });
 }
 
 /* Soehest */
+ // Tryk på søhesten for at vise dens placering //
 document.getElementById("soehest-btn").onclick = () => {
+
+    // Gemmer alle cirkler //
     hideCircles();
+
+     // Viser søhestens cirkel //
     document.getElementById("soehest-circle").style.display = "block";
 };
 
 /* Guldfisk */
+// Tryk på søhesten for at vise dens placering //
 document.getElementById("guldfisk-btn").onclick = () => {
+
+    // Gemmer alle cirkler //
     hideCircles();
+
+     // Viser søhestens cirkel //
     document.getElementById("guldfisk-circle").style.display = "block";
 };
 
+
 /* Haj */
+// Tryk på søhesten for at vise dens placering //
 document.getElementById("haj-btn").onclick = () => {
+
+    // Gemmer alle cirkler //
     hideCircles();
+
+    // Viser søhestens cirkel //
     document.getElementById("haj-circle").style.display = "block";
 };
 
 /* Blaeksprutte */
+// Tryk på søhesten for at vise dens placering //
 document.getElementById("blaeksprutte-btn").onclick = () => {
+
+    // Gemmer alle cirkler //
     hideCircles();
+
+    // Viser søhestens cirkel //
     document.getElementById("blaeksprutte-circle").style.display = "block";
 };
