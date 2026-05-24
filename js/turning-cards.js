f65c14c2ddebb4eb7c5d286d5f5018f21c3ba1f6
Anne-Sofie123 <annesofiepoulsen568@gmail.com>
2026-05-24 11:12:48 +0200

    lavet kommentar på Turning-cards-js

--- js/turning-cards.js
+++ js/turning-cards.js
@@ -1,9 +1,13 @@
+// Finder alle elementer med klassen "card" //
 const cards = document.querySelectorAll(".card");
 
+// Tager hvert kort og giver det en funktion //
 cards.forEach(card => {
 
+    // Når man trykker på kortet, vender det //
     card.addEventListener("click", () => {
 
+        // Skifter mellem at vende kortet om og tilbage //
         card.classList.toggle("flipped");
 
     });
